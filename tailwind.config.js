00cd6f9742b56447e76c9d072549890b17278405
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-04-25 19:13:48 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'quantum-blue': '#00e1ff',
+        'entropy-purple': '#a855f7',
+        'nano-green': '#22c55e'
+      }
+    }
+  },
+  plugins: []
+}
